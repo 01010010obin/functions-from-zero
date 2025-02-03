2510e2cbd25adf02e8dab820fc254d155377fb75
ROBIN <113242396+01010010obin@users.noreply.github.com>
2025-02-03 13:30:15 +0000

    adding files to repo

---
+++ mystatements.py
@@ -0,0 +1,8 @@
+my_list=["apple","cherry","banana"]
+my_dict={"drink":"coffe","eat":"cake"}
+
+for item in my_list:
+    print(f"my favorite fruits: {item}")
+
+for key,value in my_dict.items():
+    print(f"i love {key} {value}")
