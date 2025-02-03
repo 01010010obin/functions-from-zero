65b781f88cca7695b87a3ac9ef189b0486af24ba
ROBIN <113242396+01010010obin@users.noreply.github.com>
2025-02-03 19:35:10 +0000

    adding code to repo

--- wikibot.py
+++ wikibot.py
@@ -1,1 +1,8 @@
 print("hello")
+from wikipediaapi import Wikipedia
+
+# Wikipedia API'yi kullanarak bir sayfa yükleyin
+wiki = Wikipedia(language='en', user_agent="MyApp/1.0")
+page = wiki.page("Python programming language")
+print(page.title)
+print(page.summary)
