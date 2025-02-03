98d835d64407d1303cb8dab86484d955fc7e159c
ROBIN <113242396+01010010obin@users.noreply.github.com>
2025-02-03 20:56:58 +0000

    adding function

---
+++ test_wikibot.py
@@ -0,0 +1,12 @@
+from wikibot import search3
+
+#def test_search():
+#    assert "Deep Learning" in search3("Deep Learning",start=0,end=1)
+
+
+
+def test_search():
+    result = search3("Deep Learning", start=0, end=1)
+    assert result in search3("Deep Learning", start=0, end=1)
+
+
--- wikibot.py
+++ wikibot.py
@@ -6,3 +6,26 @@ wiki = Wikipedia(language='en', user_agent="MyApp/1.0")
 page = wiki.page("Python programming language")
 print(page.title)
 print(page.summary)
+
+def search(topic):
+    page = wiki.page(topic)
+    print(page.title)
+    print(page.summary)
+
+search("AI")
+
+def search2(topic):
+    page = wiki.page(topic)
+    print(page.title)
+    print(page.summary[0:10])
+
+search2("MLOps")
+
+def search3(topic="Deep Learning",start=0,end=1):
+    page = wiki.page(topic)
+    #print(page.title)
+    lst=page.summary.split('.')
+    sentences=".".join(lst[start:end]) + '.'
+    #print(sentences)
+    return sentences
+search3("Deep Learning",start=0,end=1)
