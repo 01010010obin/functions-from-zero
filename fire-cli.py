18043aef160e12e7e06d8908de697b5c447639c3
ROBIN <113242396+01010010obin@users.noreply.github.com>
2025-02-04 10:55:30 +0000

    update fire-cli

--- fire-cli.py
+++ fire-cli.py
@@ -1,2 +1,5 @@
 import fire 
 from mylib.bot import bot_search
+
+if __name__=='__main__':
+    fire.Fire(bot_search)
--- mylib/bot.py
+++ mylib/bot.py
@@ -1,9 +1,10 @@
 from wikipediaapi import Wikipedia
 wiki = Wikipedia(language='en', user_agent="MyApp/1.0")
 
-def bot_search(topic="LLMS"):
+def bot_search(topic="Large Language Models"):
+    wiki = Wikipedia(language='en', user_agent="MyApp/1.0")
     page = wiki.page(topic)
     #print(page.title)
     #print(page.summary)
-    result = print(f"{page.title} \n {page.summary}")
+    result = f"{page.title} \n {page.summary}"
     return result
