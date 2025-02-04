a4d3bfa614ba87dfefbea100c1bb16e1da48529f
ROBIN <113242396+01010010obin@users.noreply.github.com>
2025-02-04 11:04:40 +0000

    adding working code

--- fire-cli.py
+++ fire-cli.py
@@ -1,5 +1,5 @@
-import fire 
+import fire
 from mylib.bot import bot_search
 
-if __name__=='__main__':
+if __name__ == "__main__":
     fire.Fire(bot_search)
--- wikibot.py
+++ wikibot.py
@@ -3,43 +3,56 @@ from wikipediaapi import Wikipedia
 import click
 
 # Wikipedia API'yi kullanarak bir sayfa yükleyin
-wiki = Wikipedia(language='en', user_agent="MyApp/1.0")
-page = wiki.page("Python programming language")
-print(page.title)
-print(page.summary)
+#wiki = Wikipedia(language="en", user_agent="MyApp/1.0")
+#page = wiki.page("Python programming language")
+#print(page.title)
+#print(page.summary)
+
 
 def search(topic):
+    wiki = Wikipedia(language="en", user_agent="MyApp/1.0")
     page = wiki.page(topic)
     print(page.title)
     print(page.summary)
 
+
 search("AI")
 
+
 def search2(topic):
+    wiki = Wikipedia(language="en", user_agent="MyApp/1.0")
     page = wiki.page(topic)
     print(page.title)
     print(page.summary[0:10])
 
+
 search2("MLOps")
 
-def search3(topic="Deep Learning",start=0,end=1):
+
+def search3(topic="Deep Learning", start=0, end=1):
+    wiki = Wikipedia(language="en", user_agent="MyApp/1.0")
     page = wiki.page(topic)
-    #print(page.title)
-    lst=page.summary.split('.')
-    sentences=".".join(lst[start:end]) + '.'
-    #print(sentences)
+    # print(page.title)
+    lst = page.summary.split(".")
+    sentences = ".".join(lst[start:end]) + "."
+    # print(sentences)
     return sentences
-search3("Deep Learning",start=0,end=1)
+
+
+search3("Deep Learning", start=0, end=1)
+
 
 @click.command()
-@click.option('--topic',prompt='Wikipedia page to search',
-              help='Web page we want to search')
+@click.option(
+    "--topic", prompt="Wikipedia page to search", help="Web page we want to search"
+)
 def wiki_search(topic):
+    wiki = Wikipedia(language="en", user_agent="MyApp/1.0")
     page = wiki.page(topic)
-    #print(page.title)
-    #print(page.summary)
-    click.echo(click.style(f"{page.title} \n {page.summary}",fg='green',bg='white'))
+    # print(page.title)
+    # print(page.summary)
+    click.echo(click.style(f"{page.title} \n {page.summary}", fg="green", bg="white"))
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     wiki_search()
